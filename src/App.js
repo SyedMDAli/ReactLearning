6ba3463a2c1ce04b0ecd31ec357ffa7c66607af5
SyedMDAli <syedmohammadali@Syeds-MacBook-Air.local>
2022-06-24 11:22:31 +0530

    JSX example

--- src/App.js
+++ src/App.js
@@ -2,9 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <h1>Hello World!</h1>
+  
+  function formatName(userName){
+    return userName.toUpperCase();
+  }
+  
+  const name = "Syed Ali";
+  
+  
+
+  
+  
+  function getGreetings(userName){
+    if(userName){
+      return <h1> Hello {formatName(userName)}</h1>;
+    }else{
+      return <h1>Hello Stranger</h1>
+    }
+  }
+
+  const userImage ="/logo192.png";
+  const image = <img className="sample-class" src={userImage} alt="Logo" />;
+  const para = "Good to See You!";
+  const element = (
+    <div>
+      <h1>Hello {formatName(name)}!</h1>
+      <p>{para}</p>
+      {image}
+    </div>
   );
+
+  return element;
 }
 
 export default App;
